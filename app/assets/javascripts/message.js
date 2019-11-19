f40aabe9baaacef609ea4947f6f6496ac39d81b4
ryo94152 <ryo94152@gmail.com>
2019-11-19 15:09:02 +0900

    a
    a

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,25 @@
 $(function(){
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
-             ${message.user_name}
-           </div>
-           <div class="message__upper-info__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="message__text">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__upper-info">
-           <div class="message__upper-info__talker">
-             ${message.user_name}
-           </div>
-           <div class="message__upper-info__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="message__text">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="message__text">
+                    <div class="lower-message__content">
+                    ${message.content}
+                    </div>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
 $(function(){
   $("#new_message").on("submit", function(e){
     e.preventDefault();
@@ -56,7 +37,7 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
-      $('#message_content').val(''); //input内のメッセージを消す
+      $('form')[0].reset();
     })    
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
