637278856c102be382a1168a703304f1ce8df9d3
ryo94152 <ryo94152@gmail.com>
2019-11-20 20:09:20 +0900

    w
    w

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+    // var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img class= "lower-message__image" src= ${ message.image }>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -18,31 +18,54 @@ $(function(){
                     ${img}
                   </div>
                 </div>`
-  return html;
-  }
-$(function(){
-  $("#new_message").on("submit", function(e){
-    e.preventDefault();
-    var messege = new FormData(this);
-    var url = $(this).attr('action'); 
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: messege,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
-      $('form')[0].reset();
-    })    
-    .fail(function(data){
-      alert('エラーが発生したためメッセージは送信できませんでした。');
-    });
-    return false;
-  });
-});
+                return html;
+      }
+      $("#new_message").on("submit", function(e){
+        e.preventDefault();
+        var messege = new FormData(this);
+        var url = $(this).attr('action'); 
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: messege,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.messages').append(html);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+          $('form')[0].reset();
+          
+        })    
+        .fail(function(data){
+          alert('エラーが発生したためメッセージは送信できませんでした。');
+        });
+        return false;
+      });
+    
+      var reloadMessages = function() {
+        if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        let last_message_id = $('.message:last').data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          let insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          });
+        })
+        .fail(function() {
+          alert('error');
+        });
+      };
+    };
+      setInterval(reloadMessages, 5000);
 });
